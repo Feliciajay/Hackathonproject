dfe0ce8e4d9e458860e96f5f4bf4269877486037
unknown <felicaajayi80@yahoo.com>
2024-05-15 01:39:01 -0700

    firebase  added

---
+++ lib/constant/app_text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomTextStyle {
+  TextStyle body = TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey),
+                            
+}
---
+++ lib/seller/dashboard/dashboard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon/constant/app_color.dart';
+
+class SellerDashBoard extends StatelessWidget {
+  const SellerDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: AppColor.green,
+              ),
+              child: Column(
+                children: [Text("Total Product")],
+              ))
+        ],
+      ),
+    );
+  }
+}
